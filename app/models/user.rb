153e4c6992e0719603c7c901e2b0e1139f76325a
kkrasilov <kkrasilov@gmail.com>
2020-08-15 14:09:04 +0500

    refactoring model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,17 +22,15 @@ class User < ApplicationRecord
   def self.authenticate(email, password)
     user = find_by(email: email)
 
-    return nil unless user.present?
+    return unless user.present?
 
-    hashed_password = User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(
+    hashed_password = hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(
       password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST)
     )
 
-    return user if user.password_hash == hashed_password
-    nil
+    return unless user.password_hash == hashed_password
+    user
   end
-
-  private
 
   def self.hash_to_string(password_hash)
     password_hash.unpack('H*')[0]
@@ -42,13 +40,13 @@ class User < ApplicationRecord
 
   def encrypt_password
     if password.present?
-      self.password_salt = User.hash_to_string( OpenSSL::Random.random_bytes(16))
-      self.password_hash = User.hash_to_string( OpenSSL::PKCS5.pbkdf2_hmac(
+      self.password_salt = self.class.hash_to_string(OpenSSL::Random.random_bytes(16))
+      self.password_hash = self.class.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(
         password, password_salt, ITERATIONS, DIGEST.length, DIGEST))
     end
   end
 
   def normalize_username
-    self.username = username.downcase if username.present?
+    username&.downcase!
   end
 end

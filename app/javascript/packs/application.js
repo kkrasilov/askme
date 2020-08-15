b3b26e1929484c810595909ddef50c334b3333a3
kkrasilov <kkrasilov@gmail.com>
2020-08-16 02:26:20 +0500

    add jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,19 +9,11 @@ require("@rails/activestorage").start()
 require("channels")
 require('jquery')
 
-console.log('Hello from application.js')
-
-$(function(){
-    $('#ask-button').click(function(){
-        $('#ask-form').slideToggle(300);
-        return false;
+document.addEventListener("turbolinks:load", function () {
+  $(function () {
+    $('#ask-button').click(function () {
+      $('#ask-form').slideToggle(300);
+      return false;
     });
-});
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the ///image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-
+  });
+})

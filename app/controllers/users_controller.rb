bf3d0cb487de825cc35bc74b72edc907866b22d7
kkrasilov <kkrasilov@gmail.com>
2020-08-17 22:34:19 +0500

    add question count, answers, unanswered

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,14 +24,18 @@ class UsersController < ApplicationController
 
   def show
     @user = User.new(
-        name: 'Константин',
-        username: 'kkrasilov',
-        avatar_url: 'https://goodprogrammer.ru/system/avatars/000/015/986/0ecd71ec7c829b856d79bd086bb9e4eaf51d88c7_x300.jpg?1590828831'
-      )
+      name: 'Константин',
+      username: 'kkrasilov',
+      avatar_url: 'https://goodprogrammer.ru/system/avatars/000/015/986/0ecd71ec7c829b856d79bd086bb9e4eaf51d88c7_x300.jpg?1590828831'
+    )
     @questions = [
-      Question.new(text: 'Как дела?', created_at: Date.parse('15.08.2020')),
+      Question.new(text: 'Как дела?', answer: 'хорошо!', created_at: Date.parse('15.08.2020')),
       Question.new(text: 'В чем смысл жизни?', created_at: Date.parse('15.08.2020'))
     ]
+
+    @questions_count = @questions.count
+    @answers_count = @questions.map(&:answer).count
+    @unanswered_count = @questions_count - @answers_count
 
     @new_question = Question.new
   end

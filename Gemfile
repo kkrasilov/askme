4ef703d68d507017981212e155b788b50f4eb427
kkrasilov <kkrasilov@gmail.com>
2020-08-19 23:07:47 +0500

    add gem rails-i18n, refactor declension word

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
 gem 'sqlite3', '~> 1.4'
 gem 'webpacker', '~> 4.0'
 gem 'jquery-rails'
+gem 'rails-i18n', '~> 6.0.0'
 
 group :development, :test do
   gem 'pry-rails'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,16 +10,4 @@ module ApplicationHelper
   def fa_icon(icon_class)
     content_tag 'span', '', class: "fa fa-#{icon_class}"
   end
-
-  def question_declension
-    I18n.translate :question, :count => @questions_count
-  end
-
-  def answer_declension
-    I18n.translate :answer, :count => @answers_count
-  end
-
-  def unanswered_declension
-    I18n.translate :unanswered, :count => @unanswered_count
-  end
 end

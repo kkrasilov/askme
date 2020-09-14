4efd181d6294b8992ff727c6b05777528bc02d13
kkrasilov <kkrasilov@gmail.com>
2020-09-15 01:28:27 +0500

    add list questions for hashtag

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,1 +1,13 @@
+class HashtagsController < ApplicationController
+  before_action :load_hashtag
 
+  def show
+    @questions = @hashtag.questions
+  end
+
+  private
+
+  def load_hashtag
+    @hashtag = Hashtag.find_by!(name: params[:name])
+  end
+end

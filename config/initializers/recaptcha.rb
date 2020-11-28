70cc4ec6ea0b1392b94cfc0b08e8e58991d6cb64
kkrasilov <kkrasilov@gmail.com>
2020-11-28 18:39:23 +0500

    is the developer has not set reCAPTCHA

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key  = ENV['RECAPTCHA_ASKME_PUBLIC_KEY']
-  config.secret_key = ENV['RECAPTCHA_ASKME_PRIVATE_KEY']
+  config.site_key  = Rails.application.credentials.recaptcha.try(:fetch, :public_key).to_s
+  config.secret_key = Rails.application.credentials.recaptcha.try(:fetch, :private_key).to_s
 end
